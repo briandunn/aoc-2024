71af9702905c32751fdecd546e0e82328ab2fef3
Brian Dunn <briandunn@users.noreply.github.com>
2024-12-05 17:01:56 -0600

    1.1

--- Program.fs
+++ Program.fs
@@ -1,5 +1,31 @@
-﻿module One = // For more information see https://aka.ms/fsharp-console-apps
-  let one () = 
-    1
+﻿let readSeq reader =
+    let unfold (reader: System.IO.TextReader) =
+        match reader.ReadLine() with
+        | null -> None
+        | line -> Some(line, reader)
 
-printfn "%A" <| One.one()
+    Seq.unfold unfold reader
+
+module Regex =
+    let split pattern string =
+        System.Text.RegularExpressions.Regex.Split(string, pattern)
+        |> Array.toSeq
+
+module One = // For more information see https://aka.ms/fsharp-console-apps
+    let parse =
+      let fold ((ls, rs) as acc) line =
+        line
+        |> Regex.split "\s+"
+        |> Seq.map System.Convert.ToInt32
+        |> Seq.toList
+        |> function
+            | l :: r :: [] -> l :: ls, r :: rs
+            | _ -> acc
+      readSeq >> Seq.fold fold ([], []) >> function | (ls, rs) -> List.rev ls, List.rev rs
+
+    let one stream =
+      let (ls, rs) = parse stream
+      List.map2 (fun l r -> abs (l - r) ) (List.sort ls) (List.sort rs)
+      |> List.sum
+
+printfn "%A" <| One.one stdin
