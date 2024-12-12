a1a5095c5876bacffb98a4573eb3e04de958dfee
Brian Dunn <briandunn@users.noreply.github.com>
2024-12-12 10:58:40 -0600

    parse 9

--- Nine.fs
+++ Nine.fs
@@ -1,4 +1,24 @@
 module Nine
 
-let one lines = 0
+type File = { id: int; size: int; space: int }
+
+let parse: string seq -> File seq =
+    Seq.tryExactlyOne
+    >> Option.map String.chars
+    >> Option.defaultValue Seq.empty
+    >> Seq.chunkBySize 2
+    >> Seq.indexed
+    >> Seq.choose (function
+        | (id, [| size; space |]) ->
+            { id = id
+              size = size |> string |> int
+              space = space |> string |> int }
+            |> Some
+        | _ -> None)
+
+let one lines =
+    lines |> parse |> printfn "%A"
+
+    0
+
 let two lines = 0
