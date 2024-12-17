a71af4c8d3ad361327490ebb048cd2d7ecee3451
Brian Dunn <briandunn@users.noreply.github.com>
2024-12-16 20:37:08 -0600

    cheated. 11.2

--- Eleven.fs
+++ Eleven.fs
@@ -7,30 +7,54 @@ let parse =
 
 let isEven n = n % 2 = 0
 
-let blinkOne =
-    function
-    | 0L -> Seq.singleton 1L
-    | n when n |> string |> String.length |> isEven ->
-        n
-        |> string
-        |> String.chars
-        |> Seq.splitInto 2
-        |> Seq.map (fun chars -> new string (chars) |> int64)
-    | n -> Seq.singleton (n * 2024L)
+let blinkOne map =
+    let inc count =
+        function
+        | Some n -> Some(n + count)
+        | None -> Some count
+
+    let fold acc (stone, count) =
+        match stone with
+        | 0L -> acc |> Map.change 1L (inc count)
+        | n when n |> string |> String.length |> isEven ->
+            n
+            |> string
+            |> String.chars
+            |> Seq.splitInto 2
+            |> Seq.map (fun chars -> new string (chars) |> int64)
+            |> Seq.fold (fun acc stone -> Map.change stone (inc count) acc) acc
+        | n -> acc |> Map.change (n * 2024L) (inc count)
+
+    map |> Map.toSeq |> Seq.fold fold Map.empty
+
+
+let intoMap map =
+    let fold map stone =
+        let change =
+            function
+            | Some n -> Some(n + 1L)
+            | None -> Some 1L
+
+        Map.change stone change map
+
+    Seq.fold fold map
+
+let toMap = intoMap Map.empty
 
 let blink n =
     let rec blink' n =
         function
         | stones when n = 0 -> stones
-        | stones ->
-            stones |> Seq.map blinkOne |> Seq.concat |> blink' (n - 1)
+        | stones -> blink' (n - 1) (blinkOne stones)
 
-    blink' n
+    toMap >> blink' n >> Map.values >> Seq.sum
 
-let one: string seq -> int = parse >> blink 25 >> Seq.length
+let one: string seq -> int = parse >> blink 25 >> int
 
-// blinkOne is deterministic and must loop at some point
-let two lines =
-    let stones = parse lines
-
-    stones |> blink 75 |> Seq.length
+let two: string seq -> int =
+    // 1415754763 - too low
+    parse
+    >> blink 75
+    >> (fun count ->
+        printfn "%d" count
+        0)
