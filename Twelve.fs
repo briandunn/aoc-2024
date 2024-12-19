497acc5311d3fbe889490370b6df47f6d8692566
Brian Dunn <briandunn@users.noreply.github.com>
2024-12-19 08:56:23 -0600

    12.2

--- Twelve.fs
+++ Twelve.fs
@@ -55,10 +55,12 @@ let gatherRegions garden =
             gatherRegions (Set.difference unvisited region.plots) (region :: regions)
 
     gatherRegions allPlots []
-let one : string seq -> int =
-    let traceEdges {plots = plots} =
+
+let one: string seq -> int =
+    let traceEdges { plots = plots } =
         let fold count plot =
-            let isBlocked direction = plots |> Set.contains (neighbor plot direction) |> not
+            let isBlocked direction =
+                plots |> Set.contains (neighbor plot direction) |> not
 
             count + (directions |> Set.filter isBlocked |> Set.count)
 
@@ -69,4 +71,46 @@ let one : string seq -> int =
     >> List.map (fun region -> (traceEdges region) * (Set.count region.plots))
     >> List.sum
 
-let two : string seq -> int = (fun _ -> 0)
+let two: string seq -> int =
+
+    let findSides edges =
+        let fold found ((plot, blocked) as edge) =
+            let neighbors =
+                directions
+                |> Set.map (fun direction -> neighbor plot direction, blocked)
+                |> Set.intersect edges
+                |> Set.add edge
+
+            // printfn "plot: %A\nblockd: %A\nneighbors: %A" plot blocked neighbors
+
+            match
+                found
+                |> List.partition (fun edge -> edge |> Set.intersect neighbors |> Set.isEmpty)
+            with
+            | (rest, [ wall ]) -> (Set.unionMany [neighbors; wall]) :: rest
+            | (rest, _) -> neighbors :: rest
+
+        Set.fold fold [] edges
+
+
+    let filterEdges plots =
+        let fold edges plot =
+            let isBlocked direction =
+                plots |> Set.contains (neighbor plot direction) |> not
+
+            directions
+            |> Set.filter isBlocked
+            |> Set.map (fun blocked -> plot, blocked)
+            |> Set.union edges
+
+        Set.fold fold Set.empty plots
+
+    let countSides { plots = plots } =
+        plots |> filterEdges |> findSides |> Seq.length
+
+    Grid.fromLines id
+    >> gatherRegions
+    >> List.map (fun region ->
+        printfn "plant: %A area: %A\t sides: %A" region.plant (Set.count region.plots) (countSides region) 
+        (countSides region) * (Set.count region.plots))
+    >> List.sum
