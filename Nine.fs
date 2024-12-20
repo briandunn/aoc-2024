5f57b6624cdd25ff2272f50e0c23ac30be2091e6
Brian Dunn <briandunn@users.noreply.github.com>
2024-12-20 16:37:01 -0600

    cleanup

--- Nine.fs
+++ Nine.fs
@@ -58,126 +58,11 @@ let one: string seq -> int =
 
 
 module Two =
-    type File = { id: int; size: int }
-
-    type Sector =
-        | Gap of int
-        | File of File
-
-    let two: string seq -> int =
-        let toMap =
-            let fold (i, map) =
-                function
-                | { id = id; size = size; gap = gap } when gap = 0 ->
-                    i + size, map |> Map.add i (File { id = id; size = size })
-                | { id = id; size = size; gap = gap } ->
-                    i + size + gap, map |> Map.add i (File { id = id; size = size }) |> Map.add (i + size) (Gap gap)
-
-            Seq.fold fold (0, Map.empty) >> snd
-
-        let checksum: Map<int, Sector> -> int64 =
-            let fold sum sector =
-                function
-                | File { id = id; size = size } ->
-                    sum
-                    + (seq { int64 sector .. (int64 sector + int64 size - 1L) }
-                       |> Seq.map ((*) (int64 id))
-                       |> Seq.sum)
-                | _ -> sum
-
-            Map.fold fold 0L
-
-        let firstGap limit { size = size } =
-            Map.toSeq
-            >> Seq.takeWhile (fun (k, _) -> k < limit)
-            >> Seq.choose (function
-                | (sector, Gap gap) when gap > size -> Some(sector, Some(sector + size, Gap(gap - size)))
-                | (sector, Gap gap) when gap = size -> Some(sector, None)
-                | _ -> None)
-            >> Seq.tryHead
-
-        let lastFile stuckCount =
-            Map.toSeq
-            >> Seq.rev
-            >> Seq.choose (function
-                | (sector, File file) -> Some(sector, file)
-                | _ -> None)
-            >> Seq.skip stuckCount
-            >> Seq.tryHead
-
-        let compact disk =
-            let rec compact' stuckCount disk =
-                match lastFile stuckCount disk with
-                | None -> disk
-                | Some(srcSector, file) ->
-                    disk
-                    |> firstGap srcSector file
-                    |> function
-                        | Some(destSector, Some(gapSector, newGap)) ->
-                            disk
-                            |> Map.remove srcSector
-                            |> Map.add destSector (File file)
-                            |> Map.add gapSector newGap
-                            |> compact' stuckCount
-                        | Some(destSector, None) ->
-                            disk
-                            |> Map.remove srcSector
-                            |> Map.add destSector (File file)
-                            |> compact' stuckCount
-                        | None -> compact' (stuckCount + 1) disk
-
-            compact' 0 disk
-
-        parse
-        >> toMap
-        >> compact
-        >> checksum
-        >> (fun x ->
-            printfn "%d" x
-            0)
-
-module Two' =
     type File = { id: int; size: int; sector: int }
 
     type Disk =
         { files: File list
           gaps: Map<int, int> }
-
-    let printGaps gapMap =
-        let lastSector =
-            if Map.isEmpty gapMap then 0
-            else
-                gapMap
-                |> Map.maxKeyValue
-                |> function
-                    | (sector, _) -> sector
-
-        let iter sector =
-            match Map.tryFind sector gapMap with
-            | Some size -> '.' |> Seq.replicate size |> Seq.iter (printf "%c")
-            | None -> printf " "
-
-        seq { 0..lastSector } |> Seq.iter iter
-        printfn ""
-    let printFiles files =
-        let fold map file = Map.add file.sector file map
-        let fileMap = List.fold fold Map.empty files
-
-        let lastSector =
-            if Map.isEmpty fileMap then 0
-            else
-                fileMap
-                |> Map.maxKeyValue
-                |> function
-                    | (sector, _) -> sector
-
-        let iter sector =
-            match Map.tryFind sector fileMap with
-            | Some { id = id; size = size } -> id |> Seq.replicate size |> Seq.iter (printf "%d")
-            | None -> printf " "
-
-        seq { 0..lastSector } |> Seq.iter iter
-        printfn ""
 
     let two: string seq -> int =
         let toDisk =
@@ -223,8 +108,10 @@ module Two' =
                 |> firstGap file
                 |> function
                     | Some(destSector, Some(gapSector, newGap)) ->
-                        (gaps |> Map.add gapSector newGap |> Map.remove destSector, { file with sector = destSector } :: placed)
-                    | Some(destSector, None) -> (Map.remove destSector gaps, { file with sector = destSector } :: placed)
+                        (gaps |> Map.add gapSector newGap |> Map.remove destSector,
+                         { file with sector = destSector } :: placed)
+                    | Some(destSector, None) ->
+                        (Map.remove destSector gaps, { file with sector = destSector } :: placed)
                     | None -> (gaps, file :: placed)
 
             List.fold compact' (disk.gaps, []) disk.files |> snd
@@ -237,4 +124,4 @@ module Two' =
             printfn "%d" x
             0)
 
-let two = Two'.two
+let two = Two.two
