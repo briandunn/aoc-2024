f312fcd87625214e225eedcdf0999fac036c01d9
Brian Dunn <briandunn@users.noreply.github.com>
2024-12-20 17:15:55 -0600

    parse 13

--- Thirteen.fs
+++ Thirteen.fs
@@ -1,4 +1,27 @@
 module Thirteen
 
-let one lines = 0
+type Pt = int * int
+type Machine = { a: Pt; b: Pt; prize: Pt }
+
+let parse lines =
+    let regex = System.Text.RegularExpressions.Regex("(\d+)")
+
+    let parsePoint line =
+        match regex.Matches(line) |> Seq.map (fun m -> int m.Value) |> Seq.toArray with
+        | [| x; y |] -> Some(x, y)
+        | _ -> None
+
+    let parseMachine =
+        Array.choose parsePoint
+        >> function
+            | [| a; b; prize |] -> Some({ a = a; b = b; prize = prize })
+            | _ -> None
+
+    lines |> Seq.chunkBySize 4 |> Seq.choose (Array.take 3 >> parseMachine)
+
+let one lines =
+
+    lines |> parse |> printfn "%A"
+    0
+
 let two lines = 0
