4ec7bfc785f9384c883c7b8443ee1517bea2d541
Brian Dunn <briandunn@users.noreply.github.com>
2024-12-24 10:13:32 -0600

    14.2

--- Fourteen.fs
+++ Fourteen.fs
@@ -22,8 +22,20 @@ let (w, h) = dimensions
 let mx = w / 2
 let my = h / 2
 
+let advance
+    ({ position = (px, py)
+       velocity = (vx, vy) } as robot)
+    =
+    let x = (px + vx) % w
+    let x = if x < 0 then x + w else x
+    let y = (py + vy) % h
+    let y = if y < 0 then y + h else y
+    { robot with position = x, y }
+
 let print robots =
     let robotMap = robots |> Seq.groupBy (fun r -> r.position) |> Map.ofSeq
+
+    printf "%c[H" (char 0x1B)
 
     seq {
         for y in 0 .. (h - 1) do
@@ -38,23 +50,9 @@ let print robots =
     }
     |> Seq.iter (printf "%s")
 
-    printf "\n"
-
     robots
 
-let one lines =
-    let advance
-        ({ position = (px, py)
-           velocity = (vx, vy) } as robot)
-        =
-        let x = (px + vx) % w
-        let x = if x < 0 then x + w else x
-        let y = (py + vy) % h
-        let y = if y < 0 then y + h else y
-        { robot with position = x, y }
-
-    let robots = Seq.choose parse lines |> Seq.toList
-
+module One =
     let quadrant =
         function
         | (x, y) when x < mx && y < my -> Some 0
@@ -63,17 +61,46 @@ let one lines =
         | (x, y) when x > mx && y > my -> Some 3
         | _ -> None
 
-    let fold robots _ = Seq.map advance robots
+    let one robots =
+        let fold robots _ = Seq.map advance robots
 
-    seq { 1..100 }
-    |> Seq.fold fold (print robots)
-    |> print
-    |> Seq.countBy (fun robot -> quadrant robot.position)
-    |> Seq.fold
-        (fun product ->
-            function
-            | Some _, count -> count * product
-            | None, _ -> product)
-        1
+        seq { 1..100 }
+        |> Seq.fold fold (print robots)
+        |> print
+        |> Seq.countBy (fun robot -> quadrant robot.position)
+        |> Seq.fold
+            (fun product ->
+                function
+                | Some _, count -> count * product
+                | None, _ -> product)
+            1
 
-let two lines = 0
+module Two =
+
+    let isSymmetrical robots =
+        let map =
+            robots |> Seq.countBy (fun { position = position } -> position) |> Map.ofSeq
+
+        let reflectX (x, y) = (w - x - 1, y)
+
+        let forall position count =
+            map |> Map.tryFind (reflectX position) |> Option.exists ((=) count)
+
+        Map.forall forall map
+
+
+    let two =
+        let unfold robots =
+            let next = Seq.map advance robots
+            Some(next, next)
+
+        Seq.unfold unfold
+        >> Seq.indexed
+        >> Seq.skipWhile (snd >> isSymmetrical >> not)
+        >> Seq.head
+        >> fun (i, robots) ->
+            robots |> print |> ignore
+            i
+
+let one: string seq -> int = Seq.choose parse >> Seq.toList >> One.one
+let two: string seq -> int = Seq.choose parse >> Seq.toList >> Two.two
