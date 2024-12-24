294c286c3d157060945a308ddec1560cc1e0cd40
Brian Dunn <briandunn@users.noreply.github.com>
2024-12-23 23:29:03 -0600

    13.2

--- Thirteen.fs
+++ Thirteen.fs
@@ -1,13 +1,13 @@
 module Thirteen
 
-type Pt = int64 * int64
+type Pt = decimal * decimal
 type Machine = { a: Pt; b: Pt; prize: Pt }
 
 let parse: string seq -> Machine seq =
     let regex = System.Text.RegularExpressions.Regex("(\d+)")
 
     let parsePoint line =
-        match regex.Matches(line) |> Seq.map (fun m -> int64 m.Value) |> Seq.toArray with
+        match regex.Matches(line) |> Seq.map (fun m -> decimal m.Value) |> Seq.toArray with
         | [| x; y |] -> Some(x, y)
         | _ -> None
 
@@ -27,8 +27,8 @@ let one: string seq -> int =
 
     let permutations =
         seq {
-            for a in 0L .. 100L do
-                for b in 0L .. 100L do
+            for a in 0M .. 100M do
+                for b in 0M .. 100M do
                     yield a, b
         }
 
@@ -43,7 +43,7 @@ let one: string seq -> int =
                 // printfn "%A" machine
                 printfn "%A" x
                 Some x
-        |> Option.map (fun (a', b') -> (a' * 3L + b'))
+        |> Option.map (fun (a', b') -> (a' * 3M + b'))
 
     // 29334 - too low
     parse
@@ -58,44 +58,33 @@ let one: string seq -> int =
 let two: string seq -> int =
     let adjustPrize machine =
         { machine with
-            prize = plus machine.prize (10000000000000L, 10000000000000L) }
+            prize = plus machine.prize (10000000000000M, 10000000000000M) }
 
     let xCross (aX, aY) (bX, bY) (prizeX, prizeY) =
         // if a goes through prize, at what x do a and b cross?
-        let mA = (float aY) / (double aX)
-        let mB = (float bY) / (double bX)
-        let yInterceptA = float prizeY - mA * double prizeX
-
-        -yInterceptA / (mA - mB)
+        let mA = aY / aX
+        let mB = bY / bX
+        round ((prizeX * mA - prizeY) / (mA - mB))
 
     let solve
-        ({ a = (_, aY as a)
-           b = (_, bY as b)
-           prize = ((_, prizeY) as prize) } as machine)
+        { a = (aX, _ as a)
+          b = (bX, _ as b)
+          prize = ((prizeX, _) as prize) }
         =
         let x = xCross a b prize
-        let bPresses = x / (b |> fst |> float)
 
-        if abs (bPresses - round bPresses) > 0.00001 then
+        let remainingX = prizeX - x
+
+        if x % bX <> 0M || remainingX % aX <> 0M then
             None
         else
-            let bPresses = round bPresses
-            let crossY = bPresses * (float bY)
-            let diffY = (float prizeY) - crossY
-            let aPresses = round (diffY / (float aY))
-
-            // printfn "%A" machine
-            printfn "%A" (int64 aPresses, int64 bPresses)
-            Some(int64 aPresses, int64 bPresses)
-
-        |> Option.map (fun (a', b') -> (a' * 3L + b'))
+            Some(remainingX / aX, x / bX)
 
     parse
-    // >> Seq.map adjustPrize
-    // >> Seq.skip 10
-    // >> Seq.take 10
+    >> Seq.map adjustPrize
     >> Seq.choose solve
+    >> Seq.map (fun (a', b') -> (a' * 3M + b'))
     >> Seq.sum
     >> (fun sum ->
-        printfn "%d" sum
+        printfn "%M" sum
         0)
