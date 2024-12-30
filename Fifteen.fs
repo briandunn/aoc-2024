1ae050c73b107a945f208194dc800b8f1ea2005b
Brian Dunn <briandunn@users.noreply.github.com>
2024-12-29 23:41:15 -0600

    parse

--- Fifteen.fs
+++ Fifteen.fs
@@ -1,5 +1,50 @@
 module Fifteen
 
-let one lines = 0
-let two lines = 0
+type Pt = int * int
 
+type Tile =
+    | Wall
+    | Robot
+    | Box
+
+type Direction =
+    | N
+    | E
+    | S
+    | W
+
+type State =
+    { map: Map<Pt, Tile>
+      moves: Direction seq }
+
+let parse (lines: string seq) =
+    let parseMap (lines: string seq) =
+        seq {
+            for y, line in Seq.indexed lines do
+                for x, c in Seq.indexed line do
+                    match c with
+                    | '#' -> yield ((x, y), Wall)
+                    | '@' -> yield ((x, y), Robot)
+                    | 'O' -> yield ((x, y), Box)
+                    | _ -> ()
+        }
+        |> Map.ofSeq
+
+    let parseMove =
+        function
+        | '^' -> Some N
+        | '>' -> Some E
+        | 'v' -> Some S
+        | '<' -> Some W
+        | _ -> None
+
+    let map = lines |> Seq.takeWhile (String.length >> ((<>) 0)) |> parseMap
+    let moves = lines |> String.concat "" |> Seq.choose parseMove
+    { map = map; moves = moves }
+
+
+let one (lines: string seq) =
+    lines |> parse |> printfn "%A"
+    0
+
+let two lines = 0
