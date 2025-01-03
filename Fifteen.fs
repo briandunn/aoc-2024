e28bc4e660ce1f759ea1a4be044981c0c4df5df5
Brian Dunn <briandunn@users.noreply.github.com>
2025-01-02 19:41:59 -0600

    15.2

--- Fifteen.fs
+++ Fifteen.fs
@@ -111,7 +111,7 @@ let one: string seq -> int =
           moves = moves }
         =
         let fold (robot, map) move = tryMove robot move map
-        Seq.fold fold (robot, Map.remove robot map) moves
+        Seq.fold fold (robot, map) moves
 
     parse >> move >> checksum
 
@@ -125,7 +125,7 @@ module Two =
           robot: Pt
           moves: Direction seq }
 
-    let print ({robot = robot; map = map }: State') =
+    let print ({ robot = robot; map = map }: State') =
         let keys = Map.keys map
         let w = keys |> Seq.map fst |> Seq.max
         let h = keys |> Seq.map snd |> Seq.max
@@ -135,8 +135,8 @@ module Two =
             | pt when pt = robot -> (opened, out + "@")
             | pt ->
                 match Map.tryFind pt map with
-                | Some (Box' id) when Set.contains id opened -> (Set.remove id opened, out + "]")
-                | Some (Box' id) -> (Set.add id opened, out + "[")
+                | Some(Box' id) when Set.contains id opened -> (Set.remove id opened, out + "]")
+                | Some(Box' id) -> (Set.add id opened, out + "[")
                 | Some Wall' -> (opened, out + "#")
                 | None -> (opened, out + ".")
 
@@ -147,14 +147,74 @@ module Two =
         |> String.concat ""
         |> printf "%s"
 
+    let nextPosition (x, y) =
+        function
+        | N -> (x, y - 1)
+        | E -> (x + 1, y)
+        | S -> (x, y + 1)
+        | W -> (x - 1, y)
+
+    let moveBox id direction =
+        let map =
+            function
+            | (pt, ((Box' id') as box)) when id' = id -> (nextPosition pt direction, box)
+            | x -> x
+
+        Map.toSeq >> Seq.map map >> Map.ofSeq
+
+    let boxCoords id =
+        let fold state pt tile =
+            match tile with
+            | Box' id' when id' = id -> pt :: state
+            | _ -> state
+
+        Map.fold fold []
+
+    let rec tryMoveBox id direction map =
+        let rec tryClear map =
+            function
+            | [] -> Some map
+            | pt :: pts ->
+                let next = nextPosition pt direction
+
+                match Map.tryFind next map with
+                | Some Wall' -> None
+                | Some(Box' id') when id' <> id -> map |> tryMoveBox id' direction |> Option.bind (fun map -> tryClear map pts)
+                | _ -> tryClear map pts
+
+        map |> boxCoords id |> tryClear map |> Option.map (moveBox id direction)
+
+    let moveRobot robot direction map =
+        let next = nextPosition robot direction
+
+        match Map.tryFind next map with
+        | Some Wall' -> (robot, map)
+        | Some(Box' id) ->
+            match tryMoveBox id direction map with
+            | Some map -> (next, map)
+            | None -> (robot, map)
+        | None -> (next, map)
+
+    let move
+        (({ map = map
+            robot = robot
+            moves = moves }) as state)
+        =
+        let fold (robot, map) move = moveRobot robot move map
+        // let (robot, map) = Seq.fold fold (robot, map) (Seq.take 1 moves)
+        let (robot, map) = Seq.fold fold (robot, map) moves
+        { state with robot = robot; map = map }
+
     let doubleWidth
         (({ map = map
             robot = robot
             moves = moves }): State)
         : State' =
-        let doubleX (x,y) = (x * 2, y)
+        let doubleX (x, y) = (x * 2, y)
+
         let fold (id, map) pt =
             let ((x, y) as pt) = doubleX pt
+
             function
             | Wall -> (id, map |> Map.add pt Wall' |> Map.add (x + 1, y) Wall')
             | Box -> (id + 1, map |> Map.add pt (Box' id) |> Map.add (x + 1, y) (Box' id))
@@ -163,9 +223,25 @@ module Two =
           robot = doubleX robot
           moves = moves }
 
-    let two lines =
+    let checksum {map = map} =
+        let gps (x, y) = x + (y * 100)
 
-        lines |> parse |> doubleWidth |> print
-        0
+        let fold map pt =
+            let change =
+                function
+                | Some pt' -> min pt pt'
+                | None -> pt
+                >> Some
+
+            function
+            | Wall' -> map
+            | Box' id -> Map.change id change map
+
+        map |> Map.fold fold Map.empty |> Map.values |> Seq.sumBy gps
+
+    let two lines =
+        let state = lines |> parse |> doubleWidth
+
+        state |> move |> checksum
 
 let two = Two.two
