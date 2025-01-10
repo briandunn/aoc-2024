d7a90b9b1a1e10bde5639ac30cf159854cd6ee52
Brian Dunn <briandunn@users.noreply.github.com>
2025-01-09 23:51:59 -0600

    18.2

--- Eighteen.fs
+++ Eighteen.fs
@@ -1,36 +1,27 @@
 module Eighteen
 
 type Pt = int * int
-type Map = { walls: Pt Set; w: int; h: int }
 
 let maxInt = System.Int32.MaxValue
 
-let one lines =
-    let parse: string seq -> Pt Set =
-        let parseLine =
-            String.split ","
-            >> Seq.map int
-            >> Seq.toList
-            >> function
-                | [ x; y ] -> x, y
-                | _ -> failwith "Invalid point"
+let parseLine =
+    String.split ","
+    >> Seq.map int
+    >> Seq.toList
+    >> function
+        | [ x; y ] -> x, y
+        | _ -> failwith "Invalid point"
 
-        let fold walls line = Set.add (parseLine line) walls
-        Seq.fold fold Set.empty
+let w = 70
+let h = 70
+let finish = (w, h)
+let start = (0, 0)
 
-    let map =
-        { walls = lines |> Seq.take 1024 |> parse
-          w = 70
-          h = 70 }
-
-    let finish = (map.w, map.h)
-    let start = (0, 0)
-
-    let inBounds (x, y) =
-        x >= 0 && x <= map.w && y >= 0 && y <= map.h
+let dijkstra walls =
+    let inBounds (x, y) = x >= 0 && x <= w && y >= 0 && y <= h
 
     let neighbors (x, y) =
-        map.walls
+        walls
         |> Set.difference (
             [ (x + 1, y); (x - 1, y); (x, y + 1); (x, y - 1) ]
             |> List.filter inBounds
@@ -39,8 +30,7 @@ let one lines =
 
     let rec loop q scores =
         match PQ.tryPop q with
-        | None ->
-          Map.tryFind finish scores
+        | None -> Map.tryFind finish scores
         | Some(_, pt) when pt = finish -> Map.tryFind finish scores
         | Some(q, pt) ->
             let here = Map.find pt scores
@@ -57,7 +47,32 @@ let one lines =
             let q, steps = pt |> neighbors |> Set.fold fold (q, scores)
             loop q steps
 
-    loop (PQ.push 0 start Map.empty) (Map.add start 0 Map.empty) |> printfn "%A"
-    0
+    loop (PQ.push 0 start Map.empty) (Map.add start 0 Map.empty)
 
-let two lines = 0
+module One =
+    let parse: string seq -> Pt Set =
+
+        let fold walls line = Set.add (parseLine line) walls
+        Seq.fold fold Set.empty
+
+    let one = Seq.take 1024 >> parse >> dijkstra
+
+let one: string seq -> int = One.one >> Option.defaultValue -1
+
+let two lines =
+    let parse: string seq -> Pt list = Seq.map parseLine >> Seq.toList
+
+    let walls = parse lines
+
+    let rec loop min max =
+        let mid = (min + max) / 2
+
+        if min = max then
+            min
+        else
+            match walls |> List.take mid |> Set.ofList |> dijkstra with
+            | None -> loop min (mid - 1)
+            | Some _ -> loop (mid + 1) max
+
+    walls |> List.item (walls |> List.length |> loop 1024) |> printfn "%A"
+    0
